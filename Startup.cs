ade95001e62aacfe13825d6ca6dd6a84d402f53a
linkeGHub <lin.ke@live.com>
2020-09-22 22:18:15 -0500

    changed name

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Rui!");
+                    await context.Response.WriteAsync("Hello Kaiping!");
                 });
             });
         }
